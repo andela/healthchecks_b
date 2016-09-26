909f03825efd5d1dbcb477d0a8b3b3ff873abb82
kimobrian <brian.kimokoti@andela.com>
2016-09-26 21:28:13 +0300

    Resolve Merge Conflicts

--- static/js/checks.js
+++ static/js/checks.js
@@ -1,91 +1,91 @@
 $(function () {
 
-    // var MINUTE = {name: "minute", nsecs: 60};
-    // var HOUR = {name: "hour", nsecs: MINUTE.nsecs * 60};
-    // var DAY = {name: "day", nsecs: HOUR.nsecs * 24};
-    // var WEEK = {name: "week", nsecs: DAY.nsecs * 7};
-    // var UNITS = [WEEK, DAY, HOUR, MINUTE];
+    var MINUTE = {name: "minute", nsecs: 60};
+    var HOUR = {name: "hour", nsecs: MINUTE.nsecs * 60};
+    var DAY = {name: "day", nsecs: HOUR.nsecs * 24};
+    var WEEK = {name: "week", nsecs: DAY.nsecs * 7};
+    var UNITS = [WEEK, DAY, HOUR, MINUTE];
 
-    // var secsToText = function(total) {
-    //     var remainingSeconds = Math.floor(total);
-    //     var result = "";
-    //     for (var i=0, unit; unit=UNITS[i]; i++) {
-    //         if (unit === WEEK && remainingSeconds % unit.nsecs != 0) {
-    //             // Say "8 days" instead of "1 week 1 day"
-    //             continue
-    //         }
+    var secsToText = function(total) {
+        var remainingSeconds = Math.floor(total);
+        var result = "";
+        for (var i=0, unit; unit=UNITS[i]; i++) {
+            if (unit === WEEK && remainingSeconds % unit.nsecs != 0) {
+                // Say "8 days" instead of "1 week 1 day"
+                continue
+            }
 
-    //         var count = Math.floor(remainingSeconds / unit.nsecs);
-    //         remainingSeconds = remainingSeconds % unit.nsecs;
+            var count = Math.floor(remainingSeconds / unit.nsecs);
+            remainingSeconds = remainingSeconds % unit.nsecs;
 
-    //         if (count == 1) {
-    //             result += "1 " + unit.name + " ";
-    //         }
+            if (count == 1) {
+                result += "1 " + unit.name + " ";
+            }
 
-    //         if (count > 1) {
-    //             result += count + " " + unit.name + "s ";
-    //         }
-    //     }
+            if (count > 1) {
+                result += count + " " + unit.name + "s ";
+            }
+        }
 
-    //     return result;
-    // }
+        return result;
+    }
 
-    // var periodSlider = document.getElementById("period-slider");
-    // noUiSlider.create(periodSlider, {
-    //     start: [20],
-    //     connect: "lower",
-    //     range: {
-    //         'min': [60, 60],
-    //         '33%': [3600, 3600],
-    //         '66%': [86400, 86400],
-    //         '83%': [604800, 604800],
-    //         'max': 2592000,
-    //     },
-    //     pips: {
-    //         mode: 'values',
-    //         values: [60, 1800, 3600, 43200, 86400, 604800, 2592000],
-    //         density: 4,
-    //         format: {
-    //             to: secsToText,
-    //             from: function() {}
-    //         }
-    //     }
-    // });
+    var periodSlider = document.getElementById("period-slider");
+    noUiSlider.create(periodSlider, {
+        start: [20],
+        connect: "lower",
+        range: {
+            'min': [60, 60],
+            '33%': [3600, 3600],
+            '66%': [86400, 86400],
+            '83%': [604800, 604800],
+            'max': 2592000,
+        },
+        pips: {
+            mode: 'values',
+            values: [60, 1800, 3600, 43200, 86400, 604800, 2592000],
+            density: 4,
+            format: {
+                to: secsToText,
+                from: function() {}
+            }
+        }
+    });
 
-    // periodSlider.noUiSlider.on("update", function(a, b, value) {
-    //     var rounded = Math.round(value);
-    //     $("#period-slider-value").text(secsToText(rounded));
-    //     $("#update-timeout-timeout").val(rounded);
-    // });
+    periodSlider.noUiSlider.on("update", function(a, b, value) {
+        var rounded = Math.round(value);
+        $("#period-slider-value").text(secsToText(rounded));
+        $("#update-timeout-timeout").val(rounded);
+    });
 
 
-    // var graceSlider = document.getElementById("grace-slider");
-    // noUiSlider.create(graceSlider, {
-    //     start: [20],
-    //     connect: "lower",
-    //     range: {
-    //         'min': [60, 60],
-    //         '33%': [3600, 3600],
-    //         '66%': [86400, 86400],
-    //         '83%': [604800, 604800],
-    //         'max': 2592000,
-    //     },
-    //     pips: {
-    //         mode: 'values',
-    //         values: [60, 1800, 3600, 43200, 86400, 604800, 2592000],
-    //         density: 4,
-    //         format: {
-    //             to: secsToText,
-    //             from: function() {}
-    //         }
-    //     }
-    // });
+    var graceSlider = document.getElementById("grace-slider");
+    noUiSlider.create(graceSlider, {
+        start: [20],
+        connect: "lower",
+        range: {
+            'min': [60, 60],
+            '33%': [3600, 3600],
+            '66%': [86400, 86400],
+            '83%': [604800, 604800],
+            'max': 2592000,
+        },
+        pips: {
+            mode: 'values',
+            values: [60, 1800, 3600, 43200, 86400, 604800, 2592000],
+            density: 4,
+            format: {
+                to: secsToText,
+                from: function() {}
+            }
+        }
+    });
 
-    // graceSlider.noUiSlider.on("update", function(a, b, value) {
-    //     var rounded = Math.round(value);
-    //     $("#grace-slider-value").text(secsToText(rounded));
-    //     $("#update-timeout-grace").val(rounded);
-    // });
+    graceSlider.noUiSlider.on("update", function(a, b, value) {
+        var rounded = Math.round(value);
+        $("#grace-slider-value").text(secsToText(rounded));
+        $("#update-timeout-grace").val(rounded);
+    });
 
 
     $('[data-toggle="tooltip"]').tooltip();
