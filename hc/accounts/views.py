5cd13bd7a5cb606df8cabacc9b093fdd280b316f
andela-jmuli <joseph.muli@andela.com>
2016-09-29 11:06:36 +0300

    [Feature] Add Request Data handlers
    Add daily and weekly request data handlers and email triggers
    Modify template to facilitate Daily and Weekly Report Options

--- hc/accounts/views.py
+++ hc/accounts/views.py
@@ -159,6 +159,21 @@ def profile(request):
                 profile.reports_allowed = form.cleaned_data["reports_allowed"]
                 profile.save()
                 messages.success(request, "Your settings have been updated!")
+
+        elif "update_weekly_reports_allowed" in request.POST:
+            form = WeeklyReportsForm(request.POST)
+            if form.is_valid():
+                profile.weekly_reports_allowed = form.cleaned_data["weekly_reports_allowed"]
+                profile.save()
+                messages.success(request, "Your settings have been updated")
+
+        elif "update_daily_reports_allowed" in request.POST:
+            form = DailyReportSettingsForm(request.POST)
+            if form.is_valid():
+                profile.daily_reports_allowed = form.cleaned_data["daily_reports_allowed"]
+                profile.save()
+                messages.success(request, "Your settings have been updated")
+
         elif "invite_team_member" in request.POST:
             if not profile.team_access_allowed:
                 return HttpResponseForbidden()
